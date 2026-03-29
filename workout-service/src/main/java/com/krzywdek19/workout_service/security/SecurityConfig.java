ca019850559f887918e8f66ba2eb9cd0e2018044
Jakub Krzywdziński <61578292+Krzywdek19@users.noreply.github.com>
2026-03-29 17:49:26 +0200

    Add security filter chain configuration

--- workout-service/src/main/java/com/krzywdek19/workout_service/security/SecurityConfig.java
+++ workout-service/src/main/java/com/krzywdek19/workout_service/security/SecurityConfig.java
@@ -1,9 +1,38 @@
 package com.krzywdek19.workout_service.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String[] swaggerWhitelist = {
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/webjars/**",
+                "/actuator/health"
+        };
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(swaggerWhitelist).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
