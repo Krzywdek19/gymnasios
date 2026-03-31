192f63d959cd8dd58b83ba881fd2680791181277
=Jakub <Krzywdun@wp.pl>
2026-03-31 09:46:35 +0200

    fix

--- workout-service/src/main/java/com/krzywdek19/workout_service/security/SecurityConfig.java
+++ workout-service/src/main/java/com/krzywdek19/workout_service/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.krzywdek19.workout_service.security;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,20 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-=======
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
->>>>>>> ca019850559f887918e8f66ba2eb9cd0e2018044
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-<<<<<<< HEAD
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -33,21 +21,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-=======
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         String[] swaggerWhitelist = {
                 "/swagger-ui.html",
                 "/swagger-ui/**",
@@ -58,16 +31,16 @@ public class SecurityConfig {
                 "/actuator/health"
         };
 
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(swaggerWhitelist).permitAll()
                         .anyRequest().authenticated()
                 )
-                .build();
->>>>>>> ca019850559f887918e8f66ba2eb9cd0e2018044
+                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
-
